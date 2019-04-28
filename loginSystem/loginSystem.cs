6c534a1ef6cb2455bc4387c16ef39932cc1edbac
Slvr99 <bsawii@gmail.com>
2019-04-27 20:17:54 -0400

    Add project files.

---
+++ loginSystem/loginSystem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InfinityScript;
+
+namespace loginSystem
+{
+    public class loginSystem : BaseScript
+    {
+        private static Dictionary<string, string> logins = new Dictionary<string, string>();
+        public loginSystem()
+        {
+            logins.Add("Slvr99", "TestPassword");
+            logins.Add("LA|Bubbles", "Bububalo");
+        }
+
+        public override string OnPlayerRequestConnection(string playerName, string playerHWID, string playerXUID, string playerIP, string playerSteamID, string playerXNAddress)
+        {
+            if (!logins.Keys.Contains(playerName))
+                return "You have not registered for this server. Please register at www.lemonparty.com";
+
+            GSCFunctions.SetDvar("g_password", logins[playerName]);
+            return null;
+        }
+    }
+}
